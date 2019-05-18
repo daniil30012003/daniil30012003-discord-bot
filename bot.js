453d40486dfabc672630f4815829703216fd7e0a
daniil30012003 <39269156+daniil30012003@users.noreply.github.com>
2019-05-18 15:47:22 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
- 
+// other shit idk
+const ownerID = "224298948897144835";
+const prefix = "d>"
+
+// commands lol
 client.on('ready', () => {
 	client.user.setStatus('online');
 	client.user.setPresence({ game: { name: 'DONT @ ME' }, status: 'dnd' });
@@ -9,9 +13,27 @@ client.on('ready', () => {
  
 client.on('message', (message) => {
 	if (message.author.bot) return;
+	if (message.content.indexOf(prefix) !== 0) return;
+	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+	const command = args.shift().toLowerCase();
+	if (command === "b") {
+		message.channel.send("b");
+	}
 	if (message.content.includes(message.mentions.client)) {
 		message.channel.reply('FUCJ OFF'); 
 		message.delete(3000);
+	}
+	// admin commands?
+	if (message.author.id !== ownerID) return;
+	if (command === "ban") {
+		let member = message.mentions.members.first();
+		let reason = args.slice(1).join("\");
+		member.ban(reason);
+	} else
+	if (command === "send") {
+		let text = args.join("\");
+		message.delete();
+		message.channel.send(text);
 	}
 });
  
