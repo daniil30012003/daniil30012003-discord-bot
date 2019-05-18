91038c9ed128a0bd1d1a3c801f3bad0c8ca92493
daniil30012003 <39269156+daniil30012003@users.noreply.github.com>
2019-05-18 15:00:59 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,16 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
  
-client.on("ready", () => {
-	client.user.setStatus("online");
-	client.user.setGame("DON'T @ ME");
-	console.log("H");
+client.on('ready', () => {
+	client.user.setStatus('online');
+	client.user.setGame('DON'T @ ME');
+	console.log('H');
 });
  
-client.on("message", (message) => {
+client.on('message', (message) => {
 	if (message.author.bot) return;
 	if (message.content.includes(message.mentions.client)) {
-		message.channel.reply("FUCJ OFF"); 
+		message.channel.reply('FUCJ OFF'); 
 		message.delete(3000);
 	}
 });
