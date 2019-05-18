b1f8756a18186dd7d29cbad1e8c86850c9183f38
daniil30012003 <39269156+daniil30012003@users.noreply.github.com>
2019-05-18 14:57:36 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,8 +2,6 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
  
 client.on("ready", () => {
-	client.user.setUsername("daniil30012003");
-	client.user.setAvatar("https://cdn.discordapp.com/avatars/224298948897144835/022324de2d8401b3450ef43f23fa6d2d.png?size=2048");
 	client.user.setStatus("online");
 	client.user.setGame("DON'T @ ME");
 	console.log("H");
