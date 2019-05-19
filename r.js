2441661818618957e4f2e97bf264c354edd5d5ea
daniil30012003 <39269156+daniil30012003@users.noreply.github.com>
2019-05-19 10:09:32 +0000

    Create new file

---
+++ r.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+ 
+client.on("ready", () => {
+	client.user.setStatus("online")
+	client.user.setPresence({
+        	game: {
+			name: 'DON\'T @ ME',
+			type: 0
+		}
+	});
+	console.log("H!");
+});
+ 
+client.on("message", (message) => {
+	if (message.isMentioned(client.user)) {
+		message.channel.reply('FUCJ OFF'); 
+		message.delete(3000);
+	}
+});
+ 
+client.login(process.env.BOT_TOKEN);
