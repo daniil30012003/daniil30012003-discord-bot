b0b2a2fafc5c98a5e76b92ede70d2c3d4d86c563
daniil30012003 <39269156+daniil30012003@users.noreply.github.com>
2019-05-19 14:25:17 +0000

    Update r.js

--- r.js
+++ r.js
@@ -1,23 +1,27 @@
-const Discord = require("discord.js");
+// START //
+const Discord = require('discord.js');
 const client = new Discord.Client();
- 
-client.on("ready", () => {
-	client.user.setStatus("online")
+// MODULE //
+const vars = require('./var.js')
+// BOT //
+client.on('ready', () => {
+	client.user.setStatus('online')
 	client.user.setPresence({
         	game: {
 			name: 'DON\'T @ ME',
 			type: 0
 		}
 	});
-	console.log("H!");
+	console.log('H!');
 });
- 
-client.on("message", (message) => {
+// COMMAND //
+client.on('message', (message) => {
 	if (message.isMentioned(client.user)) {
-		message.reply('FUCJ OFF').then(message => {
+		message.reply(vars.msgreply[Math.floor(Math.random() * fortunes.length).then(message => {
 			message.delete(3000)
 		})
 	}
 });
- 
+// LOGIN //
 client.login(process.env.BOT_TOKEN);
+// END //
