9645c63f1dc03d3b37b559b2a03deb49f0af1b11
daniil30012003 <39269156+daniil30012003@users.noreply.github.com>
2019-05-19 06:27:45 +0000

    Create new file

---
+++ config.lua
@@ -0,0 +1,10 @@
+ownerID = "224298948897144835"
+adminID = {
+	"0",
+	"0",
+}
+botstatus = "online" -- online, idle, dnd, offline.
+gamestatus = "d>help"
+prefix = "d>"
+adminprefix = "d>>"
+argumentsplit = "\"
